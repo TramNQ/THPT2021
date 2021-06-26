8956cfddb3e2ac8a5f96538a25cb305b5bb65385
TramNQ <quoctramm@gmail.com>
2021-06-26 13:43:01 +0700

    1.1

--- script.js
+++ script.js
@@ -1,88 +1,33 @@
-/* --------------------------
- * GLOBAL VARS
- * -------------------------- */
-// The date you want to count down to
-var targetDate = new Date("2021/12/25 00:00:00");   
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-// Other date related variables
-var days;
-var hrs;
-var min;
-var sec;
+  let birthday = "Sep 30, 2021 00:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
 
-/* --------------------------
- * ON DOCUMENT LOAD
- * -------------------------- */
-$(function() {
-   // Calculate time until launch date
-   timeToLaunch();
-  // Transition the current countdown from 0 
-  numberTransition('#days .number', days, 1000, 'easeOutQuad');
-  numberTransition('#hours .number', hrs, 1000, 'easeOutQuad');
-  numberTransition('#minutes .number', min, 1000, 'easeOutQuad');
-  numberTransition('#seconds .number', sec, 1000, 'easeOutQuad');
-  // Begin Countdown
-  setTimeout(countDownTimer,1001);
-});
+        let now = new Date().getTime(),
+            distance = countDown - now;
 
-/* --------------------------
- * FIGURE OUT THE AMOUNT OF 
-   TIME LEFT BEFORE LAUNCH
- * -------------------------- */
-function timeToLaunch(){
-    // Get the current date
-    var currentDate = new Date();
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-    // Find the difference between dates
-    var diff = (currentDate - targetDate)/1000;
-    var diff = Math.abs(Math.floor(diff));  
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
 
-    // Check number of days until target
-    days = Math.floor(diff/(24*60*60));
-    sec = diff - days * 24*60*60;
+          headline.innerText = "It's my birthday!";
+          countdown.style.display = "none";
+          content.style.display = "block";
 
-    // Check number of hours until target
-    hrs = Math.floor(sec/(60*60));
-    sec = sec - hrs * 60*60;
-
-    // Check number of minutes until target
-    min = Math.floor(sec/(60));
-    sec = sec - min * 60;
-}
-
-/* --------------------------
- * DISPLAY THE CURRENT 
-   COUNT TO LAUNCH
- * -------------------------- */
-function countDownTimer(){ 
-    
-    // Figure out the time to launch
-    timeToLaunch();
-    
-    // Write to countdown component
-    $( "#days .number" ).text(days);
-    $( "#hours .number" ).text(hrs);
-    $( "#minutes .number" ).text(min);
-    $( "#seconds .number" ).text(sec);
-    
-    // Repeat the check every second
-    setTimeout(countDownTimer,1000);
-}
-
-/* --------------------------
- * TRANSITION NUMBERS FROM 0
-   TO CURRENT TIME UNTIL LAUNCH
- * -------------------------- */
-function numberTransition(id, endPoint, transitionDuration, transitionEase){
-  // Transition numbers from 0 to the final number
-  $({numberCount: $(id).text()}).animate({numberCount: endPoint}, {
-      duration: transitionDuration,
-      easing:transitionEase,
-      step: function() {
-        $(id).text(Math.floor(this.numberCount));
-      },
-      complete: function() {
-        $(id).text(this.numberCount);
-      }
-   }); 
-};
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
